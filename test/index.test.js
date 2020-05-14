65b859ce1eca7f39c8be816ee3e1c0fa8a9f0bc2
Holden Whitehead <28017008+HW13@users.noreply.github.com>
2020-05-14 11:00:41 -0700

    Add test to confirm existence of plugin opts bug.

--- test/index.test.js
+++ test/index.test.js
@@ -73,6 +73,49 @@ test('Should initialize plugin with default configuration', async ({ teardown, i
   is(response.statusCode, 200)
 })
 
+// This test should throw an error -> "this._reporter.setProcess is not a function"
+test('Should initialize plugin with user configuration', async ({ teardown, is, has }) => {
+  delete require.cache[require.resolve('jaeger-client')]
+  delete require.cache[require.resolve(require('path').join(__dirname, '../index'))]
+  require('jaeger-client')
+  const initTracerSpy = spy(require.cache[require.resolve('jaeger-client')].exports.initTracer)
+  require.cache[require.resolve('jaeger-client')].exports.initTracer = initTracerSpy
+
+  const fastify = createFastify()
+
+  teardown(async () => {
+    delete require.cache[require.resolve('jaeger-client')]
+    delete require.cache[require.resolve(require('path').join(__dirname, '../index'))]
+    await fastify.close()
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.code(200).send({ hello: 'world' })
+  })
+
+  fastify.register(require('../index'), { serviceName: 'test',  reporter: { logSpans: true } })
+
+  await fastify.ready()
+
+  const response = await fastify.inject({ method: 'GET', url: '/' })
+  is(initTracerSpy.spy.calls.length, 1)
+  has(initTracerSpy.spy.calls[0][0], {
+    serviceName: 'test',
+    sampler: { type: 'const', param: 1 },
+    reporter: { logSpans: true },
+    state: undefined
+  })
+
+  has(initTracerSpy.spy.calls[0][1], {
+    state: undefined,
+    serviceName: 'test'
+  })
+
+  is(fastify.hasRequestDecorator('jaeger'), true)
+  is(initTracerSpy.spy.calls[0][1].logger, fastify.log)
+  is(response.statusCode, 200)
+})
+
 test('Should set proper tag values', async ({ teardown, same }) => {
   const fastify = createFastify()
   const route = '/test'
